08371671597e5bfbde37a079668acaacd4533f78
dolaku <dolaku@gmail.com>
2019-03-23 16:11:35 -0400

    Able to pick only one hero

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,119 @@
 $(document).ready(function () {
-    var hero = false;
-    var villain = false;
+    var gameStart = false;
+    var heroChosen = false;
+    var villainChosen = false;
 
     var game = {
         character: [
             {
-                name: 'iceCream',
+                id: 'ice-cream',
+                name: 'Ice Cream',
                 health: 100,
                 attack: 10,
                 counterAttack: 20
             }, {
-                name: 'apple',
+                id: 'apple',
+                name: 'Apple',
                 health: 140,
                 attack: 12,
                 counterAttack: 15
             }, {
-                name: 'burger',
+                id: 'burger',
+                name: 'Burger',
                 health: 120,
                 attack: 6,
                 counterAttack: 5
             }, {
-                name: 'avocado',
+                id: 'avocado',
+                name: 'Avocado',
                 health: 180,
                 attack: 8,
                 counterAttack: 10
             }
-        ],
-        selectHero: function () {
-            $('.charGroup').on('click', function () {
-                //moves hero to bottom of screen && removes hover effects
-                $(this).appendTo('#hero').addClass('heroPlayer');
-                $(this).find('img').removeClass('selection');
-                $(this).find('.hpBadge').addClass('hpBadge-hero').removeClass('hpBadge');
-                $(this).addClass('charGroup-hero').removeClass('charGroup');
+        ]
+    }
 
-                //setup opponent selection
-                $('#topText').html('Choose Your <span class="text-danger">Opponent');
-                // $('#charWrapper').removeClass('flex-wrap');
-                $('.selection').addClass('evil').removeClass('selection');
-                $('.hpBadge').addClass('hpBadge-evil');
+    /*
+    for (var i = 0; i <character.length[i]; i++)
+    
+    */
 
-                
-                hero = true;
-            });
-        },
-        selectVillain: function () {
-            $('.charGroup').on('click', function () {
-                $(this).removeClass('charGroup').addClass('charGroup-villain');
-                $(this).appendTo('#villain').addClass('villainPlayer');
+    // if (!gameStart) {
+    //     console.log('lets start');
+    //     if (!heroChosen) {
+    //         console.log('no hero selected yet');
+    //         selectHero();
 
-                $('.charGroup')
-            });
+    //     }
+    // } else {
+    //     console.log('already in a game');
+    // }
+
+    // if (heroChosen && !villainChosen) {
+    //     selectVillain();
+    // }
+
+    // ----- FUNCTIONALITY ----- 
+    
+    // 1. Display characters to Select -- created in HTML
+    $('.char-group').on('click', function (hero) {
+        // Pick hero if one isn't already chosen
+        if ( !heroChosen ) {
+            //     moves hero into Hero div && removes hover effects
+            $(this).appendTo('#hero').addClass('hero-player');
+            $(this).find('img').removeClass('selection');
+            $(this).find('.hp-badge').addClass('hp-badge-hero').removeClass('hp-badge');
+            $(this).addClass('char-group-hero').removeClass('char-group');
+        
+            hero = $(this).attr('id');
+            $('#your-hero').html('You\'re playing as<br>' + hero.toUpperCase());
+        
+        // 2. Keep selection remove extras
+        // 3. Place extras in 'Enemies' section -- 
+        //      a. enemies remain in selection div && turns red
+        //      b. setup opponent selection
+            $('#top-text').html('Choose Your <span class="text-danger">Opponent');
+            $('.selection').addClass('evil').removeClass('selection');
+            $('.hp-badge').addClass('hp-badge-evil');
+        
+            heroChosen = true;
+            console.log('hero selected: ' + $('#hero'));
+            console.log(heroChosen);
         }
-    }
+    });
+
+    $('.char-group').on('click', function (villain) {
+        if ( !villainChosen ) {
+
+        }
+    // 4. Stage defender
+    //      a. Villain moves to Defender
+    //      b. remaining enemies stay && shrink
+        // $('.char-group').removeClass('selection');
+        // $(this).removeClass('char-group').removeClass('evil').addClass('char-group-villain');
+        // $(this).appendTo('#villain').addClass('villain-player');
+    });
 
 
-    if ( !hero ) {
-        game.selectHero();
-    }
-    if ( hero && !villain ) {
-        game.selectVillain();
-    }
+
+
+
+
 });
+
+
+// 5. Display attack button
+// 6.  ATTACK
+//     a. compare attack rating to defender rating
+//     b. determine damage
+//     c. update Attack results
+//     d. update HP
+//     e. check outcome
+//     f. Update display
+//         i. is attacker defeated
+//             a. Game Over
+//             b. Allow user to reset - display restart button
+//         ii. is defender defeated
+//             a. remove defender
+//             b. allow new enemy selection - if there isn't on already selected
+
